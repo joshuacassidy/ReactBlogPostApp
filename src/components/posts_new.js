44988083898f4133b61060746dbe08db1f042ec3
joshuacassidy <Joshcassidy79@gmail.com>
2017-05-12 19:59:58 +0100

    added react form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,12 +1,38 @@
 import React, {Component} from 'react';
+import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component{
 
   render(){
     return(
-        <div> Create Form </div>
-    )
+      <form>
+
+          <h3>Create A New Post </h3>
+          <div className="form-group">
+            <label >Title </label>
+            <input type="text" className="form-control"/>
+          </div>
+
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control"/>
+        </div>
+
+        <div className="form-group">
+          <label>Content</label>
+          <textarea type="text" className="form-control"/>
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+
+      </form>
+
+    );
   }
 
 }
-export default PostsNew;
+//recording the users input on these fields
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title','categories','content']
+}) (PostsNew);
